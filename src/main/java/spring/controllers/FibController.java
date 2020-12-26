75c8f58677ba66c45d7d63156376f9970bee13ae
ilya_kas <ilya02.mail@gmail.com>
2020-12-26 12:20:07 +0300

    first task. russian still doesnt work. <a> tag doesn't work

--- src/main/java/spring/controllers/FibController.java
+++ src/main/java/spring/controllers/FibController.java
@@ -4,22 +4,40 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/first")
 public class FibController {
+    private final Set<Integer> numbers = new HashSet<>(Arrays.asList(1,2,3,5));
 
     @GetMapping("/hello")
     public String firstPage(){
         return "first/hello";
     }
 
-    /*@GetMapping("/goodbye")
-    public String goodbyePage(@RequestParam(value = "double",required = false) Double x,
-                              @RequestParam(value = "str") String s){
-        System.out.println(x+" "+s);
-        return "first/goodbye";
-    }*/
+    @GetMapping("/problem")
+    public String taskPage(@RequestParam(value = "x") int x, Model model){
+        if (x==8)
+            return "first/next";
+        if (!numbers.contains(x)){
+            return "first/error";
+        }
+        switch (x){
+            case 1:model.addAttribute("next",2);break;
+            case 2:model.addAttribute("next",3);break;
+            default:model.addAttribute("next",5);
+        }
+        switch (x){
+            case 1:
+            case 2:
+            case 3:model.addAttribute("number", x);break;
+            case 5:model.addAttribute("number", "И куда теперь?");break;
+        }
+        return "first/problem";
+    }
 }
