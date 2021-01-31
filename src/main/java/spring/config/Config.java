47262418e36ff3756bd70e926d01ee8cdab19f19
ilya_kas <ilya02.mail@gmail.com>
2021-01-31 22:44:45 +0300

    разметил часть 4

--- src/main/java/spring/config/Config.java
+++ src/main/java/spring/config/Config.java
@@ -5,7 +5,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -20,8 +22,14 @@ public class Config implements WebMvcConfigurer {
     private final ApplicationContext applicationContext;
 
     @Autowired
+
     public Config(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
     @Bean
